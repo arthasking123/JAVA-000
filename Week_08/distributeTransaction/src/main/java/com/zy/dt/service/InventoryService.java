8a88b8e98d8505a59876c768170963c88ee0804d
arthasking123 <arthasking@126.com>
2020-12-09 17:34:40 +0800

    update

--- Week_08/distributeTransaction/src/main/java/com/zy/dt/service/InventoryService.java
+++
@@ -1,9 +0,0 @@
-package com.zy.dt.service;
-
-/**
- * Created by zy on 2020/12/9.
- */
-
-public interface InventoryService {
-
-}
--- Week_08/distributeTransaction/src/main/java/com/zy/dt/service/impl/InverntoryServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.zy.dt.service.impl;
-
-/**
- * Created by zy on 2020/12/9.
- */
-public class InverntoryServiceImpl {
-}
